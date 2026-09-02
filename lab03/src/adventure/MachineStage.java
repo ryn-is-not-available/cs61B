74f33511a45806c627d076f8f2ea884565ac1e7d
ryn-is-not-available <rayen.massek@insat.ucar.tn>
2026-09-02 14:58:11 +0100

    Fixed the advanture game

--- lab03/src/adventure/MachineStage.java
+++ lab03/src/adventure/MachineStage.java
@@ -112,8 +112,8 @@ public class MachineStage implements AdventureStage {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = mysteryMax(a[i], b[i]);
-            returnArray[i] = biggerValue;
+            int biggerValue = mysteryMax(-a[i], -b[i]);
+            returnArray[i] = -biggerValue;
         }
 
         return returnArray;
@@ -126,7 +126,7 @@ public class MachineStage implements AdventureStage {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + mysteryAdd(sum, x[i]);
+            sum =mysteryAdd(sum, x[i]);
             i = i + 1;
         }
         return sum;
